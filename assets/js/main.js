778303aefbbad1f46c6eb688d197bb595b38c32b
Signior-X <b19188@students.iitmandi.ac.in>
2020-07-21 10:40:31 +0530

    Solved isotope problem using strict mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -128,24 +128,23 @@ window.addEventListener('load', function() {
 
   var portfolioList = document.getElementById('portfolio-flters').getElementsByTagName('li');
   // console.log(portfolioList);
-  for(let i=0; i< portfolioList.length; i++){
-    var thisOne = portfolioList[i];
-    thisOne.addEventListener('click', function(){
-      // Remove the class from everyone
-      for(var j=0; j<portfolioList.length; j++){
-        portfolioList[j].classList.remove('filter-active');
-      }
-      thisOne.classList.add('filter-active');
-
-      
-      portfolioIsotope.arrange({
-        filter: thisOne.getAttribute('data-filter')
+  for (let i=0; i< portfolioList.length; i++) {
+    (function () {
+      var thisOne = portfolioList[i];
+      thisOne.addEventListener('click', function(){
+        // Remove the class from everyone
+        for(var j=0; j<portfolioList.length; j++){
+          portfolioList[j].classList.remove('filter-active');
+        }
+        thisOne.classList.add('filter-active');
+  
+        portfolioIsotope.arrange({
+          filter: thisOne.getAttribute('data-filter')
+        });
+        aos_init();
       });
-
-      aos_init();
-    });
+    }()); //Immediate invocation
   }
-
 
 });
 
